b42b5d8fc0603d02d15b84fae9505002795ab337
Artem Usov <artem.o.u@gmail.com>
2022-12-11 19:21:05 +0000

    Day 4

--- main.cpp
+++ main.cpp
@@ -1,99 +1,47 @@
+#include "absl/strings/numbers.h"
+#include "absl/strings/str_split.h"
 #include <fstream>
 #include <iostream>
-#include <optional>
-#include <set>
+#include <string>
 
-constexpr char UPPER_BASE = 38;
-constexpr char LOWER_BASE = 96;
-
-std::optional<char> findCommon(std::string_view first, std::string_view second) {
-  std::set<char> set;
-
-  for (const char c : first) {
-    set.emplace(c);
-  }
-
-  for (const char c : second) {
-    if (set.contains(c)) {
-      return c;
-    }
-  }
-
-  return std::nullopt;
-}
-
-std::optional<char> findCommons(std::string_view first, std::string_view second, std::string_view third) {
-  std::set<char> first_set;
-  std::set<char> second_set;
-
-  for (const char c : first) {
-    first_set.emplace(c);
-  }
-
-  for (const char c : second) {
-    if (first_set.contains(c)) {
-      second_set.emplace(c);
-    }
-  }
-
-  for (const char c : third) {
-    if (second_set.contains(c)) {
-      return c;
-    }
-  }
-
-  return std::nullopt;
-}
-
-std::int64_t first() {
+int main() {
   std::fstream file{"data.txt"};
   std::string line;
 
-  std::int64_t sum = 0;
+  std::int64_t count_complete = 0;
+  std::int64_t count_partial = 0;
 
   while (std::getline(file, line)) {
-    const std::string_view line_sv{line};
-    auto half = line_sv.size() / 2;
-    auto opt_char = findCommon(line_sv.substr(0, half), line_sv.substr(half, half));
+    const std::pair<absl::string_view, absl::string_view> regions = absl::StrSplit(line, ',');
 
-    if (opt_char.has_value()) {
-      auto c = opt_char.value();
-      if (isupper(c) != 0) {
-        sum += c - UPPER_BASE;
-      } else {
-        sum += c - LOWER_BASE;
+    const std::pair<absl::string_view, absl::string_view> elf1_str = absl::StrSplit(regions.first, '-');
+    std::pair<std::int64_t, std::int64_t> elf1{0, 0};
+    absl::SimpleAtoi(elf1_str.first, &elf1.first);
+    absl::SimpleAtoi(elf1_str.second, &elf1.second);
+
+    const std::pair<absl::string_view, absl::string_view> elf2_str = absl::StrSplit(regions.second, '-');
+    std::pair<std::int64_t, std::int64_t> elf2{0, 0};
+    absl::SimpleAtoi(elf2_str.first, &elf2.first);
+    absl::SimpleAtoi(elf2_str.second, &elf2.second);
+
+    if (elf2.first < elf1.first) {
+      std::swap(elf1, elf2);
+    } else if (elf2.first == elf1.first) {
+      if (elf2.second > elf1.second) {
+        std::swap(elf1, elf2);
       }
+    }
+
+    std::cout << elf1.first << " " << elf1.second << " " << elf2.first << " " << elf2.second << std::endl;
+
+    if (elf1.second >= elf2.second) {
+      ++count_complete;
+    }
+
+    if (elf2.first <= elf1.second) {
+      ++count_partial;
     }
   }
-  return sum;
+
+  std::cout << "Overlaps: " << count_complete << " partial: " << count_partial << std::endl;
 }
-
-std::int64_t second() {
-  std::fstream file{"data.txt"};
-  std::string elf1;
-  std::string elf2;
-  std::string elf3;
-
-  std::int64_t sum = 0;
-
-  do {
-    std::getline(file, elf1);
-    std::getline(file, elf2);
-    std::getline(file, elf3);
-
-    auto opt_char = findCommons(elf1, elf2, elf3);
-
-    if (opt_char.has_value()) {
-      auto c = opt_char.value();
-      if (isupper(c) != 0) {
-        sum += c - UPPER_BASE;
-      } else {
-        sum += c - LOWER_BASE;
-      }
-    }
-  } while (!file.fail());
-
-  return sum;
-}
-
-int main() { std::cout << "First problem: " << first() << " Second Problem: " << second() << std::endl; }
